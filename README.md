1f0bc1e957c0d793cd4f3125508a640616b2af99
brunoleo223 <brunoleo223@gmail.com>
2022-09-20 23:13:15 -0300

    reademe + instrucoes na index.js

--- README.md
+++ README.md
@@ -1,34 +1,23 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Next.js Documentação em Portugês 🇧🇷
 
-## Getting Started
+Esse projeto busca traduzir e manter atualizada a documentação oficial do Next.js para Português Brasil.
 
-First, run the development server:
+## Próximas etapas
+- [ ] Configurar layout de base
+- [ ] Carregar arquivos markdown da documentação em Inglês
+- [ ] Listagem dos links do menu
+- [ ] Configurar docs/[slug].js
+- [ ] Traduzir todos os 139 arquivos markdown para Português
+- [ ] Atualizar links internos
+- [ ] Configurar roteamento da página inicial e /docs
+- [ ] Programar novo layout 
 
-```bash
-npm run dev
-# or
-yarn dev
-```
+## Contribua</h2>
+Para contribuir é simples! Você pode:
+- Traduzir páginas
+- Implementar novos componentes
+- Melhorar a performance
+- Implemtnar novas funcionalidades
+- Criar novos layouts e estilos
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
-
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.js`.
-
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+Contribua: [Link do repositório](https://github.com/brunoleo223/next-documentacao-pt-br)
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,62 @@
-import fs from 'fs';
-import path from 'path';
-import matter from 'gray-matter';
-import Menu from '../components/Menu';
-
-function App({posts}) {
+function App() {
   return (
     <>
-      <h1>adf</h1>
+      <h1>Next.js Documentação em Portugês 🇧🇷</h1>
+      <p>Esse projeto busca traduzir e manter atualizada a documentação oficial do Next.js para Português Brasil.</p>
+      <h2>Próximas etapas</h2>
+      <ul>
+        <li>
+          <label htmlFor="item1">
+            <input type="checkbox" checked id="item1" /> Configurar layout de base
+          </label>
+        </li>
+        <li>
+          <label htmlFor="item2">
+            <input type="checkbox" checked id="item2" /> Carregar arquivos markdown da documentação em Inglês
+          </label>
+        </li>
+        <li>
+          <label htmlFor="item3">
+            <input type="checkbox" checked id="item3" /> Listagem dos links do menu
+          </label>
+        </li>
+        <li>
+          <label htmlFor="item4">
+            <input type="checkbox" checked id="item4" /> Configurar docs/[slug].js
+          </label>
+        </li>
+        <li>
+          <label htmlFor="item5">
+            <input type="checkbox" id="item5" /> Traduzir todos os 139 arquivos markdown para Português
+          </label>
+        </li>
+        <li>
+          <label htmlFor="item6">
+            <input type="checkbox" id="item6" /> Atualizar links internos
+          </label>
+        </li>
+        <li>
+          <label htmlFor="item7">
+            <input type="checkbox" id="item7" /> Configurar roteamento da página inicial e /docs
+          </label>
+        </li>
+        <li>
+          <label htmlFor="item8">
+            <input type="checkbox" id="item8" /> Programar novo layout 
+          </label>
+        </li>
+      </ul>
+      <h2>Contribua</h2>
+      <p>Para contribuir é simples! Você pode:</p>
+      <ul>
+        <li>Traduzir páginas</li>
+        <li>Implementar novos componentes</li>
+        <li>Melhorar a performance</li>
+        <li>Implemtnar novas funcionalidades</li>
+        <li>Criar novos layouts e estilos</li>
+      </ul>
+      <p>Contribua: <span><a href="https://github.com/brunoleo223/next-documentacao-pt-br" rel="noreferrer" target="_blank">Link do repositório</a></span></p>
     </>
   )
 }
 export default App
-
-export async function getStaticProps() {
-  const files = fs.readdirSync(path.join('posts'));
-  const posts = files.map(filename => {
-    const slug = filename.replace('.md', '');
-    const markdownWithMeta = fs.readFileSync(
-      path.join('posts', filename), 'utf-8');
-      
-    const {data: frontmatter} = matter(markdownWithMeta);
-    return {
-      slug,
-      frontmatter
-    }
-  })
-
-  return {
-    props: {
-      posts
-    }
-  }
-}
